f3600c349db5ab1bd1dd38ae834cbc6181adce9c
AzAlex <99219332+azalex020204@users.noreply.github.com>
2023-04-02 16:52:21 +0300

    Add files via upload

---
+++ uiCard/js/main.js
@@ -0,0 +1,27 @@
+
+// =======================typing animation========================
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "HTML",
+    "CSS / SCSS",
+    "TailwindCSS",
+    "Bootstrap",
+    "JS",
+    "JQuery",
+    "React.JS",
+    "Node.JS",
+    "Figma",
+    "Adobe Photoshop",
+    "Adobe Premier PRO",
+    "SVG Animation",
+    "Responsive",
+    "Parallax Scrolling",
+    "Mobile First",
+    "BEM",
+  ],
+  typeSpeed: 150,
+  BackSpeed: 2000,
+  loop: true,
+});
+ 
